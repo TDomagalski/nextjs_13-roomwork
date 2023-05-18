60bfe072509ed7174850bbeb179427b5812ac047
TDomagalski <tomekwebdev@gmail.com>
2023-05-18 19:59:18 +0200

    feat(eslint): install eslint and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'airbnb',
+		'plugin:jsx-a11y/recommended',
+		'plugin:@next/next/recommended',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'jsx-a11y'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+		'react/jsx-one-expression-per-line': 'off',
+		'react/prop-types': 'off',
+		indent: [0, 'tab'],
+		'no-tabs': 0,
+	},
+};
