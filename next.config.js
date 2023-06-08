f7f68706d9e1f0bb4a7d6d818eab5f783e56a203
TDomagalski <tomekwebdev@gmail.com>
2023-06-09 00:29:21 +0200

    fix: images unoptimized: true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 const path = require('path');
 
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
